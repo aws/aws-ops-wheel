fbbba39ef9a3a8668ab13085610cfa53f5924648
Brandon Schwartz <bsschwar@amazon.com>
2022-05-24 14:55:22 -0500

    Fixing tests so they pass again. Mostly updates from boto APIs, updating mock wheel table to have the proper index.

--- api/conftest.py
+++ api/conftest.py
@@ -14,8 +14,8 @@
 import os
 import pytest
 from boto3.session import Session
-from moto import mock_dynamodb2
-with mock_dynamodb2():
+from moto import mock_dynamodb as ddb_mock
+with ddb_mock():
     from utils import add_extended_table_functions
     import utils
 
@@ -23,14 +23,13 @@ WHEEL_TABLE_NAME = os.environ.get('WHEEL_TABLE', 'DevOpsWheel-Wheels')
 PARTICIPANT_TABLE_NAME = os.environ.get('PARTICIPANT_TABLE', 'DevOpsWheel-Participants')
 
 
-@pytest.yield_fixture(scope='session')
+@pytest.fixture(scope='session')
 def mock_dynamodb():
 
-    mock_dynamodb2().start()
+    ddb_mock().start()
 
     session = Session(aws_access_key_id='<ACCESS_KEY_ID>',
-                      aws_secret_access_key='<SECRET_KEY>',
-                      region_name='us-west-2')
+                      aws_secret_access_key='<SECRET_KEY>')
     dynamodb = session.resource('dynamodb')
 
     wheel_table = dynamodb.create_table(
@@ -45,12 +44,30 @@ def mock_dynamodb():
             {
                 'AttributeName': 'id',
                 'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'name',
+                'AttributeType': 'S'
             }
         ],
         ProvisionedThroughput={
             'ReadCapacityUnits': 1,
             'WriteCapacityUnits': 1
-        }
+        },
+        GlobalSecondaryIndexes=[{
+            'IndexName': 'name_index',
+            'KeySchema': [{
+                'AttributeName': 'name',
+                'KeyType': 'HASH'
+            }],
+            'Projection': {
+                'ProjectionType': 'ALL'
+            },
+            'ProvisionedThroughput': {
+                'ReadCapacityUnits': 1,
+                'WriteCapacityUnits': 1
+            }
+          }]
     )
 
     participant_table = dynamodb.create_table(
@@ -87,7 +104,7 @@ def mock_dynamodb():
 
     yield dynamodb
 
-    mock_dynamodb2().stop()
+    ddb_mock().stop()
 
 
 @pytest.fixture
@@ -96,7 +113,7 @@ def mock_wheel_table(mock_dynamodb):
     add_extended_table_functions(Wheel)
     utils.Wheel = Wheel
     yield Wheel
-    wheels = Wheel.scan({})['Items']
+    wheels = Wheel.scan()['Items']
     with Wheel.batch_writer() as batch:
         for wheel in wheels:
             batch.delete_item(Key={'id': wheel['id']})
@@ -108,7 +125,7 @@ def mock_participant_table(mock_dynamodb):
     add_extended_table_functions(WheelParticipant)
     utils.WheelParticipant = WheelParticipant
     yield WheelParticipant
-    participants = WheelParticipant.scan({})['Items']
+    participants = WheelParticipant.scan()['Items']
     with WheelParticipant.batch_writer() as batch:
         for participant in participants:
             batch.delete_item(Key={'id': participant['id'], 'wheel_id': participant['wheel_id']})
